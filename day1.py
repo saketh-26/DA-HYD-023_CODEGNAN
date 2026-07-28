f26a58f6305235d168a91ee6eb8ab0bd72bea56a
Saketh Kallepu <sakethkallepu@Sakeths-MacBook-Air.local>
2026-07-28 13:18:40 +0530

    Tokens-Day1

---
+++ day1.py
@@ -0,0 +1,93 @@
+#IDLE is Color Coding Environment
+#hastag is used as Single Line comment...
+#We use triple quotes (single/double) ''',""" as Multiline comments
+#print('hello codegnan')
+
+'''
+Tokens --> A smallest unit in a program.There are 5 types of tokens..
+
+->Keywords
+->identifiers
+->Literals
+->Operators
+->Punctuators
+
+#Keywords -->These are reserved words in Python .We have 35 Keywords...
+#help() -->keywords -->it gives list of 35 keywords -->True,False.......
+
+#identifiers -->These are the names being alloted to variables,functions,classes...
+#We cannot start with a number.
+#We can start with Uppercase /lowercase letters (A-Z,a-z) but not special characters
+#@,#,$,.....
+#No space between letters,where as we can also use underscore as an identifier _
+#Python is a case-sensitive language
+name = 'Codegnan'
+print(name)
+email_id ='saketh@codegnan.com'
+#print(Email_id) #Python is Case-Sensitive -->NameError
+#1swer = 23
+#print(1swer)
+#@defrrrrr = 3.4 #No special characters
+
+#True = 45 #No keywords
+
+batch = "da23"
+print(batch)
+
+#variable --> named memory location where we store the data,its a placeholder...
+batch = 'cse'
+print(batch)
+
+batch = 23
+print(batch)
+
+#Literals -->These are constants (value(s)) -->number,character
+
+#Operators --> Operators perform specific operation -->7types
+
+length = 5
+breadth = 3
+area = length * breadth
+print(area) 
+'''
+
+#Multiassignment of variables
+name = 'codegnan'
+print(name)
+
+name,location,age = 'Codegnan','Hyd',7
+print(name)
+print(location)
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
