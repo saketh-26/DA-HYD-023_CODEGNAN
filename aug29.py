4b4a8c078af5b2db69c1843ad6b09cb1c4568661
saketh-26 <saketh@codegnan.com>
2026-08-29 07:56:23 +0000

    IntroductiontoModules

---
+++ aug29.py
@@ -0,0 +1,18 @@
+'''
+POP --> Functions (*args & **kwargs),Exception Handling
+OOP --> Inheritance,Polymorphism,Encapsulation,Abstraction
+Modules --> A Module is a python file containing variables,
+functions,objects --> import  keyword will be used...
+'''
+def greeting():
+    """Simple Greeting Function"""
+    return "Welcome to Codegnan"
+
+
+details = {'company':'Codegnan','batch':'DA-23','place':'Hyd'}
+
+if __name__ == "__main__":
+    print(greeting())
+
+print(__name__)
+#print(greeting())
